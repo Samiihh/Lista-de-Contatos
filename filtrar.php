ba195d768ad65983a8a34b903fa311ddad258717
Samira Vieira <101117402+Samira-Vieira@users.noreply.github.com>
2023-09-01 09:49:20 -0300

    Alteração no filtro

--- filtrar.php
+++ filtrar.php
@@ -3,10 +3,11 @@
 include_once "conexao.php";
 
 // Obter a data do filtro
-$filtroData = $_POST['filtroData'];
+$filtroData = $_POST['filtroData']; 
+
 
 // Consultar o banco de dados para registros com data de cadastro ou data de alteração igual à data do filtro
-$query_filter = "SELECT * FROM contatos WHERE DATE(datacadastro) = :filtroData OR DATE(dataalteracao) = :filtroData";
+$query_filter = "SELECT id,nome,datacadastro,dataalteracao FROM contatos WHERE DATE(datacadastro) = :filtroData OR DATE(dataalteracao) = :filtroData";
 
 $query_filter = $conn->prepare($query_filter);
 $query_filter->bindParam(':filtroData', $filtroData);
@@ -14,21 +15,34 @@ $query_filter->bindParam(':filtroData', $filtroData);
 $query_filter->execute();
 
 $resultados = $query_filter->fetchAll(PDO::FETCH_ASSOC);
-var_dump($_POST);
-var_dump($resultados);
-die;
+
+//Cabecalho da table
+$tabelaHTML = "<div class='table-responsive'>
+<table class='table table-striped table-bordered'>
+    <thead>
+        <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Data Cadastro</th>
+        <th>Data Alteracao</th>
+        </tr>
+    </thead>
+    <tbody>";
 
 // Construa uma tabela HTML com os resultados e retorne-a como resposta
-$tabelaHTML = '<table>'; // Construa a tabela HTML com base nos resultados
 foreach ($resultados as $registro) {
+
     $tabelaHTML .= '<tr>';
     $tabelaHTML .= '<td>' . $registro['id'] . '</td>';
     $tabelaHTML .= '<td>' . $registro['nome'] . '</td>';
-    $tabelaHTML .= '<td>' . $registro['data_cadastro'] . '</td>';
-    $tabelaHTML .= '<td>' . $registro['data_alteracao'] . '</td>';
+    $tabelaHTML .= '<td>' . $registro['datacadastro'] . '</td>';
+    $tabelaHTML .= '<td>' . $registro['dataalteracao'] . '</td>';
     $tabelaHTML .= '</tr>';
 }
-$tabelaHTML .= '</table>';
+
+$tabelaHTML .= "</tbody>
+</table>
+</div>";
 
 echo $tabelaHTML; // Retorne a tabela HTML como resposta
 
